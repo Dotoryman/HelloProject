64aa0b65b82a9ecc2f70f2c5283c28801ec43128
admin <admin@DESKTOP-63ML1K3>
2023-06-05 13:07:36 +0900

    2023 06 05 오전

--- HelloApp2/src/com/yedam/insa/EmpList.java
+++ HelloApp2/src/com/yedam/insa/EmpList.java
@@ -4,6 +4,15 @@ import java.util.Scanner;
 
 // 사원정보 배열, 등록/조회/출력 정의
 public class EmpList {
+	
+	
+	//싱글턴
+	private static EmpList instance = new EmpList();
+	
+	public static EmpList getInstance() {
+		return instance;
+	}
+	
 	//정보저장공간
 	
 	Employee[] list;
@@ -27,6 +36,44 @@ public class EmpList {
 	
 	//등록
 	public void input() {
+		if (empNum >= list.length) {
+			System.out.println("입력이 초과되었습니다.");
+			return;
+		}
 		System.out.printf("%d 사번 > ", empNum);
+		int no = scn.nextInt();
+		System.out.printf("이름 > ");
+		String name = scn.next(); // 엔터까지 처리된다
+		System.out.printf("급여 > ");
+		int sal = scn.nextInt();
+		list[empNum++] = new Employee(no, name, sal);
+	}
+	
+	//조회
+	public String search(int employeeId) {
+		for( int i = 0 ; i < list.length; i++) {
+			if(list[i] != null && list[i].getEmployeeId() == employeeId) {
+				return list[i].getName();
+			}
+		}
+		return "";
+	}
+	
+	//전체출력
+	public void print() {
+		System.out.println("==사원번호======성명=====급여=====");
+		for(int i = 0 ; i < list.length; i++) {
+			System.out.printf("%5d %10s %7d \n", list[i].getEmployeeId(), list[i].getName(), list[i].getSalary());
+			
+		}
+	}
+	
+	//급여 합계
+	public int sum() {
+		int sum = 0;
+		for(int i = 0 ; i < list.length; i++) {
+			sum += list[i].getSalary();
+		}
+		return sum;
 	}
 }
